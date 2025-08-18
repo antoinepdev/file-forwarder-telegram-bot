14909c014252d9cf6f427d976dd45fbcf3ac2401
* <antoinepenadev@gmail.com>
2025-08-18 18:58:55 -0400

    improve the functioning of environment variables
    Automatically terminate the bot if any of the environment variables are
    missing

--- bot_config.py
+++ bot_config.py
@@ -2,9 +2,25 @@ from telebot import TeleBot
 from dotenv import load_dotenv
 from os import getenv
 
-load_dotenv(".env")
+env_file = ".env"
+load_dotenv(env_file)
 bot_token = getenv("BOT_TOKEN")
-storage_id = int(getenv("STORAGE_ID"))
+storage_id_str = getenv("STORAGE_ID")
 bot_link = getenv("BOT_LINK")
 
-bot = TeleBot(bot_token)
+
+
+def env_error(varname):
+    raise Exception(f"{varname} not provaided. Please add {varname} variable into your {env_file} file")
+
+if bot_token == None:
+    env_error("BOT_TOKEN")
+if storage_id_str == None:
+    env_error("STORAGE_ID")
+if bot_link == None:
+    env_error("BOT_LINK")
+
+
+
+storage_id = int(storage_id_str) #type: ignore
+bot = TeleBot(bot_token) #type: ignore
