fff101d21b73b151e6ee0117977f55fd7250a90d
* <antoinepenadev@gmail.com>
2025-08-18 19:09:11 -0400

    print what mode the user is in depending on their env file
    The bot prints to the console "production mode" or "development mode"
    depending on the name of the env file used
    
    ".env" => prod mode
    any other name, for example ".env.dev" => dev mode

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from bot_config import bot
+from bot_config import bot, env_file
 import id_asigner
 import forwarder
 
 if __name__ == "__main__":
+    if env_file == ".env": print("prod mode actived")
+    else: print("dev mode actived")
     print("works...")
     bot.infinity_polling()
